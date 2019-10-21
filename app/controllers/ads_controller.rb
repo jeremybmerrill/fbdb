3c412d8e3fa013376b6de446071c8112fba70830
Jeremy B. Merrill <jmerrill@qz.com>
2019-10-21 17:29:39 -0400

    adds page filtering

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -3,59 +3,75 @@ require 'elasticsearch/dsl'
 
 class AdsController < ApplicationController
 
-	def show
-		if params[:archive_id]
-			@ad = Ad.find_by(archive_id: params[:archive_id]) 
-		elsif params[:ad_id]
-			@ad = Ad.find(ad_id: params[:ad_id])
-		end
+    def show
+        if params[:archive_id]
+            @ad = Ad.find_by(archive_id: params[:archive_id]) 
+        elsif params[:ad_id]
+            @ad = Ad.find(ad_id: params[:ad_id])
+        end
 
 
-		@fbpac_ad = @ad.fbpac_ad
+        @fbpac_ad = @ad.fbpac_ad
 
 
-		respond_to do |format|
-		  format.html
-		  format.json { render json: {
-		  	ad: @ad.as_json(include: :fbpac_ad)
-		  } }
-		end
-	end
+        respond_to do |format|
+          format.html
+          format.json { render json: {
+            ad: @ad.as_json(include: :fbpac_ad)
+          } }
+        end
+    end
 
-	def index
-		# eventually the search method?
-		@ads = Ad.includes(:fbpac_ad).paginate(page: params[:page], per_page: 30)
+    def index
+        # eventually the search method?
+        @ads = Ad.includes(:fbpac_ad).paginate(page: params[:page], per_page: 30)
 
-		respond_to do |format|
-			format.html 
-			format.json { render json: @ads.as_json(include: :fbpac_ad) }
-		end
-	end
+        respond_to do |format|
+            format.html 
+            format.json { render json: @ads.as_json(include: :fbpac_ad) }
+        end
+    end
 
-	def search
-		# keywordsearch: ad text
-		# keywordsearch URL?
-		# some sort of search UTM params
-		# keywordsearch: targeting 
-		# filter: disclaimer, advertiser
-		# keywordsearch disclaimers?
-		# time based filter
-		search = params[:search]
+    def search
+        # keywordsearch: ad text
+        # keywordsearch URL?
+        # some sort of search UTM params
+        # keywordsearch: targeting 
+        # filter: disclaimer, advertiser
+        # keywordsearch disclaimers?
+        # time based filter
 
-		query = Elasticsearch::DSL::Search.search do
-		  query do
-		  	multi_match do 
-		      query search if search
-		      fields [:text, :payer_name, :page_name]
-		  	end
-		    # TODO: targeting, if we end up getting it.
-		    # TODO: filter by  states seen, impressions minimums/maximums, topics
-		  end
-		end
-		@ads = Ad.search query
-		respond_to do |format|
-			format.html 
-			format.json { render json: @ads.as_json(include: :fbpac_ad) }
-		end
-	end
+        
+        search = params[:search]
+        page_id = params[:page_id]
+        publish_date = nil # "2019-01-01"
+
+        puts page_id
+
+        query = Elasticsearch::DSL::Search.search do
+          query do
+            bool do
+              must do
+                multi_match do
+                  query search
+                  fields [:text, :payer_name, :page_name]
+                end 
+              end if search
+              filter do
+                term page_id: page_id.to_i if page_id
+                range :creation_date do  
+                  gte publish_date 
+                end if publish_date
+                # TODO: targeting, if we end up getting it.
+                # TODO: filter by  states seen, impressions minimums/maximums, topics
+              end if [page_id, publish_date].any?{|a| a }
+            end
+          end
+        end
+        @ads = Ad.search query
+        respond_to do |format|
+            format.html 
+            format.json { render json: @ads.as_json(include: :fbpac_ad) }
+        end
+    end
 end
