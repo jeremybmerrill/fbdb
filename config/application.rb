4e4a246dbe5ef57c1327722d26162c2947942ab2
Brendan Hamill <bhamill@qz.com>
2020-01-29 11:26:10 -0500

    Adds http basic auth

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module AtiDashboard
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
