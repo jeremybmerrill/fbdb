136d56b710ac039f81368cc4be66a7aa4c9bd2eb
Jeremy B Merrill <jeremybmerrill@gmail.com>
2020-07-26 21:33:45 -0400

    whitespace fix

--- app/models/writable_page.rb
+++ app/models/writable_page.rb
@@ -1,5 +1,5 @@
 class WritablePage < ApplicationRecord
-	belongs_to :page, primary_key: :page_id, foreign_key: :page_id, optional: true
+  belongs_to :page, primary_key: :page_id, foreign_key: :page_id, optional: true
 
   has_many :ad_texts, primary_key: :page_id, foreign_key: :page_id
   has_many :ads, primary_key: :page_id, foreign_key: :page_id
